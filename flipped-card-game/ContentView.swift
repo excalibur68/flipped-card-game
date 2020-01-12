57cda7f5fa80fca9a32006d6bfbc0e8201866378
Xi Han <xihan94@ucla.edu>
2020-01-12 15:28:00 -0800

    Init a xcode project

---
+++ flipped-card-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  flipped-card-game
+//
+//  Created by Xi Han on 1/12/20.
+//  Copyright © 2020 Yan Cao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ flipped-card-gameTests/flipped_card_gameTests.swift
@@ -0,0 +1,34 @@
+//
+//  flipped_card_gameTests.swift
+//  flipped-card-gameTests
+//
+//  Created by Xi Han on 1/12/20.
+//  Copyright © 2020 Yan Cao. All rights reserved.
+//
+
+import XCTest
+@testable import flipped_card_game
+
+class flipped_card_gameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
